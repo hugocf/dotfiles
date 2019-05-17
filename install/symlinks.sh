ff158cccb3611909e9fbe7b5d5435d3e693b05d7
Hugo Ferreira <hugo@ferreira.cc>
2019-05-17 14:41:33 +0100

    install: Make all home symlinks absolute

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -13,21 +13,21 @@ link_files () {
     echo -e "\n=== Link config dotfiles ==="
     local path=$(relpath ../../dotfiles.git)
     ln -nfs $path ~/.dotfiles
-    ln -nfs .dotfiles/bash/bash_aliases     ~/.bash_aliases
-    ln -nfs .dotfiles/bash/bash_profile     ~/.bash_profile
-    ln -nfs .dotfiles/bash/colors.sh        ~/.colors.sh
-    ln -nfs .dotfiles/git/gitconfig         ~/.gitconfig
-    ln -nfs .dotfiles/mate/tm_properties    ~/.tm_properties
+    ln -nfs ~/.dotfiles/bash/bash_aliases   ~/.bash_aliases
+    ln -nfs ~/.dotfiles/bash/bash_profile   ~/.bash_profile
+    ln -nfs ~/.dotfiles/bash/colors.sh      ~/.colors.sh
+    ln -nfs ~/.dotfiles/git/gitconfig       ~/.gitconfig
+    ln -nfs ~/.dotfiles/mate/tm_properties  ~/.tm_properties
 }
 
 link_local () {
     echo -e "\n=== Link local dotfiles ==="
     local path=$(relpath ../../dotfiles_local.git)
     ln -nfs $path ~/.dotfiles_local
-    ln -nfs .dotfiles_local/aws             ~/.aws
-    ln -nfs .dotfiles_local/bash_history    ~/.bash_history
-    ln -nfs .dotfiles_local/github/token    ~/.github
-    ln -nfs .dotfiles_local/ssh             ~/.ssh
+    ln -nfs ~/.dotfiles_local/aws           ~/.aws
+    ln -nfs ~/.dotfiles_local/bash_history  ~/.bash_history
+    ln -nfs ~/.dotfiles_local/github/token  ~/.github
+    ln -nfs ~/.dotfiles_local/ssh           ~/.ssh
 }
 
 link_system () {
