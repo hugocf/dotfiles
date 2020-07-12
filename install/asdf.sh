1ae9113691d54ed0c290e8eee207419ac7ed4c52
Hugo Ferreira <hugo@ferreira.cc>
2020-07-12 21:14:37 +0100

    install: Review apps

--- install/asdf.sh
+++ install/asdf.sh
@@ -23,23 +23,21 @@ install () {
 }
 
 install_plugins () {
-    install golang      1.13
-    install kops        1.11.1.z
-    install kubectl     1.14.1
-    install minikube    1.0.1
-    install nodejs      12.13.0     10.17.0
     install ruby        2.6.3
-    install sbt         1.2.8
-    install terraform   0.11.13
+    install sbt         1.3.9       1.2.8
+    install terraform   0.12.28
 }
 
 setup_config () {
-    kubectl completion bash > $(brew --prefix)/etc/bash_completion.d/kubectl
-    minikube completion bash > $(brew --prefix)/etc/bash_completion.d/minikube
+    :
 }
 
 deprecate () {
-    :
+    asdf plugin-remove golang
+    asdf plugin-remove kops
+    asdf plugin-remove kubectl
+    asdf plugin-remove minikube
+    asdf plugin-remove nodejs
 }
 
 # Do the work
