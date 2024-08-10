c619bc4b0f141c4f8a80125178fe8a1cfe9dfb5e
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-08-10 08:23:29 +0100

    install: yadm aliases to verify changes in binary prefs

---
+++ custom-source.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+yadm_check_changes() {
+    yadm status --porcelain | cut -c4- | while read -r filename; do
+        if [[ "$filename" == *.plist && "$(file --brief --mime "$filename")" == application/octet-stream* ]]; then
+            pl2xml "$filename"
+        fi
+    done
+    yadm diff
+}
+
+yadm_restore_unstaged() {
+    yadm status --porcelain | grep "^.[M?]" | cut -c4- | xargs -I{} yadm restore {}
+}
+
+pl2xml() {
+    local bin="$1"
+    local xml="$1.xml"
+
+    cp "$bin" "$xml"
+    plutil -convert xml1 "$xml"
+    echo "Converted copy to xml in $xml"
+}
+
+alias plcat='plutil -convert xml1 -o -'
+alias plread=plcat
+
+alias g=git
+alias y=yadm
+alias ll='ls -lh'
