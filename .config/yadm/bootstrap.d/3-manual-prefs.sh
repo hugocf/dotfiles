5e96100af095a143ddaf527912cfe1f84a8bb33b
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-08-12 03:04:52 +0100

    prefs: Add 1Password manual settings
    Note: macOS uses Settings instead of Preferences for apps menu
    
    > System Preferences becomes System Settings and features a new design that’s optimized for efficient navigation on Mac, and delivers a more consistent experience across iPhone and iPad. 
    >
    > — [macOS Ventura is now available - Apple][1] on October 24, 2022
    
    [1]: https://www.apple.com/newsroom/2022/10/macos-ventura-is-now-available/

--- .config/yadm/bootstrap.d/3-manual-prefs.sh
+++ .config/yadm/bootstrap.d/3-manual-prefs.sh
@@ -2,9 +2,19 @@
 set -euo pipefail
 
 main() {
-    # echo -e "\n=== Preferences (manual) ==="
-    :
+    echo -e "\n=== Settings (manual) ==="
+    set_1password_settings
     echo
+}
+
+set_1password_settings() {
+    echo -e "\n--- 1Password Settings ---"
+    echo "General   ➤ Show Quick Access: ⌃ ⌥ ⌘ P"
+    echo "Security  ➤ Touch ID: ON"
+    echo "          ➤ Apple Watch: ON"
+    echo "          ➤ Hold Option to toggle revelead fields: ON"
+    echo "Privacy   ➤ Check for vulnerable passwords: ON"
+    echo "Developer ➤ Use the SSH Agent: ON"
 }
 
 main "$@"
