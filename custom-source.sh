1a8de981f4452bdc6eb5d85e39d2398c6b9e28bf
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-09-30 11:08:02 +0100

    install: Isolate yadm alias functions

--- custom-source.sh
+++ custom-source.sh
@@ -5,28 +5,6 @@ defaults_domains() {
     defaults domains | tr ',' '\n' | sort | grep -i "$filter"
 }
 
-yadm_check_changes() {
-    yadm status --porcelain | cut -c4- | while read -r filename; do
-        if [[ "$filename" == *.plist && "$(file --brief --mime "$filename")" == application/octet-stream* ]]; then
-            pl2xml "$filename"
-        fi
-    done
-    yadm diff
-}
-
-yadm_restore_unstaged_plist() {
-    yadm status --porcelain | grep "^.[M?].*plist.*" | cut -c4- | xargs -I{} yadm restore {}
-}
-
-pl2xml() {
-    local bin="$1"
-    local xml="$1.xml"
-
-    cp "$bin" "$xml"
-    plutil -convert xml1 "$xml"
-    echo "Converted copy to xml in $xml"
-}
-
 typeset -U path
 path=(~/.local/bin/scripts $path)
 
@@ -39,3 +17,4 @@ alias ll='ls -lh'
 alias lla='ll -a'
 
 [[ -f "$HOME/custom-source.work.sh" ]] && source "$HOME/custom-source.work.sh" || :
+[[ -f "$HOME/custom-source.yadm.sh" ]] && source "$HOME/custom-source.yadm.sh" || :
---
+++ custom-source.yadm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+yadm_check_changes() {
+    yadm status --porcelain | cut -c4- | while read -r filename; do
+        if [[ "$filename" == *.plist && "$(file --brief --mime "$filename")" == application/octet-stream* ]]; then
+            pl2xml "$filename"
+        fi
+    done
+    yadm diff
+}
+
+yadm_restore_unstaged_plist() {
+    yadm status --porcelain | grep "^.[M?].*plist.*" | cut -c4- | xargs -I{} yadm restore {}
+}
+
+pl2xml() {
+    local bin="$1"
+    local xml="$1.xml"
+
+    cp "$bin" "$xml"
+    plutil -convert xml1 "$xml"
+    echo "Converted copy to xml in $xml"
+}
