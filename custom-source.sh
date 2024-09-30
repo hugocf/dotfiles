6d589f1a6cc98840f94b3a6a9819336d72b54538
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-09-30 17:58:36 +0100

    shell: Configure infinite `zsh` history

--- custom-source.sh
+++ custom-source.sh
@@ -8,6 +8,22 @@ defaults_domains() {
 typeset -U path
 path=(~/.local/bin/scripts $path)
 
+# [zsh: 15 Paramenters](https://zsh.sourceforge.io/Doc/Release/Parameters.html#Parameters-Used-By-The-Shell)
+# man zshparam
+export HISTSIZE=10000000
+export SAVEHIST=$HISTSIZE
+# [zsh: 16 Options](https://zsh.sourceforge.io/Doc/Release/Options.html#History)
+# man zshoptions
+setopt EXTENDED_HISTORY
+setopt HIST_FIND_NO_DUPS
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_VERIFY
+setopt SHARE_HISTORY    # https://apple.stackexchange.com/a/427568
+# user aliases
+alias history='fc -l -iD 1 | less +G'
+alias h=history
+
 alias plcat='plutil -convert xml1 -o -'
 alias plread=plcat
 
