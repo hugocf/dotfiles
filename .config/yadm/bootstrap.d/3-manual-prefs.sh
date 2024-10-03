a2f846d0e59c46748411e61280d751a93c30e413
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-10-03 10:57:01 +0100

    prefs: Stop using password manager browser extensions
    Reduce potential attack vectors by lowering the exposure surface and
    only using the installed 1Password app.

--- .config/yadm/bootstrap.d/3-manual-prefs.sh
+++ .config/yadm/bootstrap.d/3-manual-prefs.sh
@@ -14,6 +14,7 @@ main() {
 
 all_manual_settings() {
     set_1password_settings
+    set_chrome_settings
     set_facetime_settings
     set_notes_settings
     set_shortcuts_settings
@@ -26,15 +27,25 @@ set_1password_settings() {
     echo "          ➤ ${bold}ON${reset} Apple Watch"
     echo "          ➤ ${bold}ON${reset} Hold Option to toggle revelead fields"
     echo "Privacy   ➤ ${bold}ON${reset} Check for vulnerable passwords"
+    echo "Browser   ➤ ${bold}OFF${reset} Connect with 1Password in the browser"
     echo "Developer ➤ ${bold}ON${reset} Use the SSH Agent"
     echo "          ➤ ${bold}ON${reset} Integrate with 1Password CLI"
     echo "[Create a New Password]"
     echo "          ➤ Characters: ${bold}32${reset} "
     echo "          ➤ Symbols:    ${bold}ON${reset} "
 
-    h2 "1Password Chrome Extension"
-    echo "Chrome Profile [each] ➤ chrome://extensions/shortcuts"
-    echo "                      ➤ Activate the extension: ${bold}⇧ ⌘ P${reset}"
+    h2 "1Password Chrome Extension [Installed by Your Administrator]"
+    echo "Chrome Profile [each]"
+    echo "Autofill… ➤ ${bold}OFF${reset} Offer to fill and save passwords"
+    echo "          ➤ ${bold}OFF${reset} Offer to save and sign with passkeys"
+    echo "          ➤ ${bold}OFF${reset} Allow caching passkey IDs in local storage"
+}
+
+set_chrome_settings() {
+    h2 "Chrome"
+    echo "Chrome Profile [each]"
+    echo "Autofill… ➤ [Google Password Manager](chrome://password-manager/settings)"
+    echo "Settings  ➤ ${bold}OFF${reset} Offer to save passwords and passkeys"
 }
 
 set_facetime_settings() {
