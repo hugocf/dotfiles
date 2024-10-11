6374877f6e86ee149ea36d7e21a66ac860f8580f
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-10-12 00:19:44 +0100

    shell: Add custom scripts to zsh default path

--- custom-source.sh
+++ custom-source.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env zsh
 
-typeset -U path
-path=(~/.local/bin/scripts $path)
-
 [[ -f "$HOME/custom-source.work.sh" ]] && source "$HOME/custom-source.work.sh" || :
 [[ -f "$HOME/custom-source.yadm.sh" ]] && source "$HOME/custom-source.yadm.sh" || :
