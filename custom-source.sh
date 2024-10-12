4b3d17729c18cc4baf88ba55408c151cb9c7c378
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-10-12 01:08:03 +0100

    shell: Move custom yadm functions to a separate script
    Git aliases cannot access zsh functions, only regular shell scripts:
    https://github.com/hugocf/scripts/commit/a67bf1773a700db083e759ecefdac843d9c68592

--- custom-source.sh
+++ custom-source.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env zsh
 
 [[ -f "$HOME/custom-source.work.sh" ]] && source "$HOME/custom-source.work.sh" || :
-[[ -f "$HOME/custom-source.yadm.sh" ]] && source "$HOME/custom-source.yadm.sh" || :
