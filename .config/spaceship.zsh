cd918c745aabd7c5f0b8a707cdaae9692872c986
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-10-20 19:42:10 +0100

    shell: Review and adapt the old `bash_profile`

---
+++ .config/spaceship.zsh
@@ -0,0 +1,3 @@
+# [Prompt configuration - Spaceship](https://spaceship-prompt.sh/config/prompt/)
+SPACESHIP_DIR_TRUNC=0               # Number of folders of cwd to show in prompt, 0 to show all
+SPACESHIP_PROMPT_ADD_NEWLINE=false  # Adds a newline character before each prompt line
