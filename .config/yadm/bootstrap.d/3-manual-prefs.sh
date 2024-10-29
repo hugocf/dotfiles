ea4b9b091a1cf52969933a3c0968b73a91d3e3d8
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-10-29 00:28:13 +0000

    prefs: Reformat manual settings alignment

--- .config/yadm/bootstrap.d/3-manual-prefs.sh
+++ .config/yadm/bootstrap.d/3-manual-prefs.sh
@@ -22,31 +22,32 @@ all_manual_settings() {
 
 set_1password_settings() {
     h2 "1Password"
-    echo "General   ➤ Show Quick Access: ${bold}⌃ ⌥ ⌘ P${reset}"
-    echo "Security  ➤ ${bold}ON${reset} Touch ID"
-    echo "          ➤ ${bold}ON${reset} Apple Watch"
-    echo "          ➤ ${bold}ON${reset} Hold Option to toggle revelead fields"
-    echo "Privacy   ➤ ${bold}ON${reset} Check for vulnerable passwords"
-    echo "Browser   ➤ ${bold}OFF${reset} Connect with 1Password in the browser"
-    echo "Developer ➤ ${bold}ON${reset} Use the SSH Agent"
-    echo "          ➤ ${bold}ON${reset} Integrate with 1Password CLI"
+    echo "General       ➤ Show Quick Access: ${bold}⌃ ⌥ ⌘ P${reset}"
+    echo "Security      ➤ ${bold}ON${reset} Touch ID"
+    echo "              ➤ ${bold}ON${reset} Apple Watch"
+    echo "              ➤ ${bold}ON${reset} Hold Option to toggle revelead fields"
+    echo "Privacy       ➤ ${bold}ON${reset} Check for vulnerable passwords"
+    echo "Browser       ➤ ${bold}OFF${reset} Connect with 1Password in the browser"
+    echo "Developer     ➤ ${bold}ON${reset} Use the SSH Agent"
+    echo "              ➤ ${bold}ON${reset} Integrate with 1Password CLI"
     echo "[Create a New Password]"
-    echo "          ➤ Characters: ${bold}32${reset} "
-    echo "          ➤ Symbols:    ${bold}ON${reset} "
+    echo "              ➤ Characters: ${bold}32${reset} "
+    echo "              ➤ Symbols:    ${bold}ON${reset} "
 
     h2 "1Password Chrome Extension [Installed by Your Administrator]"
     echo "Chrome Profile [each]"
-    echo "Autofill… ➤ chrome://password-manager/settings"
-    echo "          ➤ ${bold}OFF${reset} Offer to save passwords and passkeys"
-    echo "Keyboard… ➤ chrome://extensions/shortcuts"
-    echo "          ➤ Activate the extension: ${bold}Not set${reset}"
+    echo "chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa/app/app.html#/page/settings"
+    echo "Autofill …    ➤ ${bold}OFF${reset} Offer to fill and save passwords"
+    echo "              ➤ ${bold}OFF${reset} Offer save and sign in with passkeys"
+    echo "chrome://extensions/shortcuts"
+    echo "Keyboard …    ➤ Activate the extension: ${bold}Not set${reset}"
 }
 
 set_chrome_settings() {
     h2 "Chrome"
     echo "Chrome Profile [each]"
-    echo "Autofill… ➤ [Google Password Manager](chrome://password-manager/settings)"
-    echo "Settings  ➤ ${bold}OFF${reset} Offer to save passwords and passkeys"
+    echo "chrome://password-manager/settings"
+    echo "Settings      ➤ ${bold}OFF${reset} Offer to save passwords and passkeys"
 }
 
 set_facetime_settings() {
@@ -58,13 +59,13 @@ set_facetime_settings() {
 
 set_notes_settings() {
     h2 "Notes"
-    echo "Settings  ➤ Sort notes by: ${bold}Title${reset}"
-    echo "          ➤ ${bold}ON${reset} Use Touch ID [use same pwd as personal login]"
+    echo "Settings      ➤ Sort notes by: ${bold}Title${reset}"
+    echo "              ➤ ${bold}ON${reset} Use Touch ID [use same pwd as personal login]"
 }
 
 set_shortcuts_settings() {
     h2 "Shortcuts"
-    echo "Advanced  ➤ ${bold}ON${reset} Allow Running Scripts"
+    echo "Advanced      ➤ ${bold}ON${reset} Allow Running Scripts"
 }
 
 main "$@"
