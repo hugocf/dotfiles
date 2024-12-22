d7999ebc9808b4ed7df44f4c60d0755d12003dea
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-12-22 11:10:43 +0000

    yadm: Shell file formatting using VS Code extension

--- .config/yadm/bootstrap.d/2-set-defaults.sh
+++ .config/yadm/bootstrap.d/2-set-defaults.sh
@@ -22,19 +22,19 @@ system_character_palette_categories() {
     echo "Character Palette categories reset"
     # Adding to the default: MusicalSymbols, SignStandardSymbols, TechnicalSymbols
     defaults write com.apple.CharacterPaletteIM CVActiveCategories -array \
-            "Category-Emoji"                \
-            "Category-Arrows"               \
-            "Category-Bullets"              \
-            "Category-CurrencySymbols"      \
-            "Category-Latin"                \
-            "Category-LetterlikeSymbols"    \
-            "Category-MathematicalSymbols"  \
-            "Category-MusicalSymbols"       \
-            "Category-Parentheses"          \
-            "Category-Pictographs"          \
-            "Category-Punctuation"          \
-            "Category-SignStandardSymbols"  \
-            "Category-TechnicalSymbols"
+        "Category-Emoji" \
+        "Category-Arrows" \
+        "Category-Bullets" \
+        "Category-CurrencySymbols" \
+        "Category-Latin" \
+        "Category-LetterlikeSymbols" \
+        "Category-MathematicalSymbols" \
+        "Category-MusicalSymbols" \
+        "Category-Parentheses" \
+        "Category-Pictographs" \
+        "Category-Punctuation" \
+        "Category-SignStandardSymbols" \
+        "Category-TechnicalSymbols"
 }
 
 system_lock_screen_message() {
