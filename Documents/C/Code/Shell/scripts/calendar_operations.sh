a66fa1fb954e842495b71c282b7f48d2448c48df
Hugo Ferreira <hugocf@users.noreply.github.com>
2025-05-03 00:16:55 +0100

    scripts: Move main to the top of macOS Calendar operations

--- Documents/C/Code/Shell/scripts/calendar_operations.sh
+++ Documents/C/Code/Shell/scripts/calendar_operations.sh
@@ -4,45 +4,6 @@ set -euo pipefail
 readonly DB="$HOME/Library/Group Containers/group.com.apple.calendar/Calendar.sqlitedb"
 readonly NSDATE_DELTA=978307200
 
-get_events_by_url() {
-    local url="${1:-:}"
-    local sql="
-        select
-            rowid,
-            datetime(start_date + $NSDATE_DELTA, 'unixepoch', 'localtime') as date,
-            datetime(last_modified + $NSDATE_DELTA, 'unixepoch', 'localtime') as date,
-            summary,
-            url
-        from CalendarItem
-        where url like '%$url%'
-        order by start_date;
-    "
-    sqlite3 "$DB" "$sql"
-}
-
-set_events_url_by_title() {
-    local title="$1"
-    local url="$2"
-    local sql="update CalendarItem set url = '$url', last_modified = strftime('%s', 'now') - $NSDATE_DELTA where summary = '$title';"
-    sqlite3 "$DB" "$sql"
-}
-
-set_events_url_by_url() {
-    local old_url="$1"
-    local new_url="$2"
-    local sql="update CalendarItem set url = '$new_url', last_modified = strftime('%s', 'now') - $NSDATE_DELTA where url = '$old_url';"
-    sqlite3 "$DB" "$sql"
-}
-
-remove_event_alarms_by_title() {
-    local title="$1"
-    local sql="
-        delete from alarm where calendaritem_owner_id in (select rowid from CalendarItem where summary = '$title');
-        update CalendarItem set last_modified = strftime('%s', 'now') - $NSDATE_DELTA  where summary = '$title';
-    "
-    sqlite3 "$DB" "$sql"
-}
-
 main() {
     local usage="Usage: $(basename $0) command [args...]
 
@@ -80,6 +41,45 @@ This script requires sqlite3 and assumes the calendar database is located at: $D
             exit 1
             ;;
     esac
+}
+
+get_events_by_url() {
+    local url="${1:-:}"
+    local sql="
+        select
+            rowid,
+            datetime(start_date + $NSDATE_DELTA, 'unixepoch', 'localtime') as date,
+            datetime(last_modified + $NSDATE_DELTA, 'unixepoch', 'localtime') as date,
+            summary,
+            url
+        from CalendarItem
+        where url like '%$url%'
+        order by start_date;
+    "
+    sqlite3 "$DB" "$sql"
+}
+
+set_events_url_by_title() {
+    local title="$1"
+    local url="$2"
+    local sql="update CalendarItem set url = '$url', last_modified = strftime('%s', 'now') - $NSDATE_DELTA where summary = '$title';"
+    sqlite3 "$DB" "$sql"
+}
+
+set_events_url_by_url() {
+    local old_url="$1"
+    local new_url="$2"
+    local sql="update CalendarItem set url = '$new_url', last_modified = strftime('%s', 'now') - $NSDATE_DELTA where url = '$old_url';"
+    sqlite3 "$DB" "$sql"
+}
+
+remove_event_alarms_by_title() {
+    local title="$1"
+    local sql="
+        delete from alarm where calendaritem_owner_id in (select rowid from CalendarItem where summary = '$title');
+        update CalendarItem set last_modified = strftime('%s', 'now') - $NSDATE_DELTA  where summary = '$title';
+    "
+    sqlite3 "$DB" "$sql"
 }
 
 main "$@"
