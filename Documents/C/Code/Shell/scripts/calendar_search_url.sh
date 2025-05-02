c7de6faf3b08288c75e51e25d7159fb0b674bc61
Hugo Ferreira <hugocf@users.noreply.github.com>
2025-05-02 01:33:11 +0100

    scripts: Add macOS Calendar event update functions

--- Documents/C/Code/Shell/scripts/calendar_search_url.sh
+++ Documents/C/Code/Shell/scripts/calendar_search_url.sh
@@ -15,3 +15,20 @@ events="
 "
 
 sqlite3 "$db" "$events"
+
+# Archived functions
+
+set_event_url_by_title() {
+    local title="$1"
+    local url="$2"
+    local sql="update CalendarItem set url = '$url' where summary = '$title';"
+
+    sqlite3 "$db" "$sql"
+}
+
+remove_event_alarms_by_title() {
+    local title="$1"
+    local sql="delete from alarm where calendaritem_owner_id in (select rowid from CalendarItem where summary = '$title');"
+
+    sqlite3 "$db" "$sql"
+}
